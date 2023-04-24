edaaa40e1bff8887d05c09f94819c723170cc5c6
Jaan Sigel <51171863+794963@users.noreply.github.com>
2023-04-23 21:32:19 -0600

    Add files via upload

--- pager-predict.c
+++ pager-predict.c
@@ -11,13 +11,25 @@ void pageit(Pentry q[MAXPROCESSES])
             page = q[proc].pc/PAGESIZE;//get current proc page
             predArr[page] = 1;//set current proc page to 1 in prediction array
             switch(page){
+                case 3:
+                    predArr[0] = 1;
+                    predArr[4] = 1;
+                    predArr[10] = 1;
+                case 11:
+                    predArr[0] = 1;
+                    predArr[12] = 1;
+                    predArr[1] = 1;
+                case 12:
+                    predArr[0] = 1;
+                    predArr[9] = 1;
+                    predArr[13] = 1;
                 case 13:
                     predArr[0] = 1;
+                    predArr[9] = 1;
+                    predArr[10] = 1;
                     predArr[14] = 1;
-                    predArr[1] = 1;
                 case 14:
                     predArr[0] = 1;
-                    predArr[1] = 1;
                     predArr[2] = 1;
                 default:
                 //default is first page then next two as the predictor (testing with this and edge case first)
