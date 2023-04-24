bb53e7d81b1e2277a54a1e261fc3003b2716336c
Jaan Sigel <51171863+794963@users.noreply.github.com>
2023-04-23 21:18:50 -0600

    Add files via upload

--- pager-predict.c
+++ pager-predict.c
@@ -1,31 +1,38 @@
 #include <stdio.h> 
 #include <stdlib.h>
-
 #include "simulator.h"
+void pageit(Pentry q[MAXPROCESSES])
+{ 
+    int page;
+    int proc;
+    for(proc = 0; proc < MAXPROCESSES; proc++){
+        if (q[proc].active){//check proc active
+            int predArr[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//prediction array intitialization
+            page = q[proc].pc/PAGESIZE;//get current proc page
+            predArr[page] = 1;//set current proc page to 1 in prediction array
+            switch(page){
+                case 13:
+                    predArr[0] = 1;
+                    predArr[14] = 1;
+                    predArr[1] = 1;
+                case 14:
+                    predArr[0] = 1;
+                    predArr[1] = 1;
+                    predArr[2] = 1;
+                default:
+                //default is first page then next two as the predictor (testing with this and edge case first)
+                    predArr[0] = 1;
+                    predArr[page + 1] = 1;
+                    predArr[page + 2] = 1;
+        }
+            for(int i = 0; i < 15; i++){//iterate through predArr and pageout and pagein based on bit(1/0) of page
+                if (predArr[i] == 0){
+                    pageout(proc,i);//evict
+                }else{
+                    pagein(proc,i);//success
+                }
+            }
 
-void pageit(Pentry q[MAXPROCESSES]) { 
-    
-    /* This file contains the stub for a predictive pager */
-    /* You may need to add/remove/modify any part of this file */
-
-    /* Static vars */
-    static int initialized = 0;
-    static int tick = 1; // artificial time
-    
-    /* Local vars */
-    
-
-    /* initialize static vars on first run */
-    if(!initialized){
-	/* Init complex static vars here */
-	
-	initialized = 1;
+        }
     }
-    
-    /* TODO: Implement Predictive Paging */
-    fprintf(stderr, "pager-predict not yet implemented. Exiting...\n");
-    exit(EXIT_FAILURE);
-
-    /* advance time for next pageit iteration */
-    tick++;
-} 
+}
